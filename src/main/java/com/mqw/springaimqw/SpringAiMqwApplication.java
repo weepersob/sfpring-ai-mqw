c2ed01e16a2ec1281ef88f0c029dd22ecd84d94e
weepersob <2458832862@qq.com>
2025-03-31 09:17:49 +0800

    spring-ai基础使用

---
+++ src/main/java/com/mqw/springaimqw/SpringAiMqwApplication.java
@@ -0,0 +1,13 @@
+package com.mqw.springaimqw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiMqwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiMqwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mqw/springaimqw/SpringAiMqwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mqw.springaimqw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiMqwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
